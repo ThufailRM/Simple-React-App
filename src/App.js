0c118a6062203d9d50f2178bd2b5320f770213b6
Thufail RM <thufail@holoteq.com>
2023-01-17 16:30:20 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <button onClick={callApi}>Call API</button>
+        </header>
+      </div>
+  );
+}
+
+function callApi() {
+    fetch('https://simple-node-server-niru.herokuapp.com/details', { method: 'GET' })
+        .then(data => data.json())
+        .then(json => alert(JSON.stringify(json)))
+}
+
+export default App;
